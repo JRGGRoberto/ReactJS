2713af3de754fb0d75b06c6fcaacb8b819e5e799
Roberto <jrggroberto@gmail.com>
2019-10-17 18:06:30 -0300

    initial commit: completed

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+const Footer = () => (
+  <footer id="main-footer"><p>JSHunter 2019</p>
+     <p>Posted by: JRoberto Góes</p>
+     <p>Contact information: <a href="mailto:jrggroberto@gmail.com">
+     jrggroberto@gmail.com</a>.</p>
+  </footer>
+);
+
+export default Footer;
+
+
